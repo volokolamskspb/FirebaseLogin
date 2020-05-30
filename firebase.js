021d3472e2c4691fccb9d9ea193e033307fc2dc1
Yulia <>
2020-05-31 00:02:58 +0300

    firebas

---
+++ firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBGgKKuuIAmeBRQI3QPVw59IfI6VSmCohI",
+    authDomain: "my-project-1574068747683.firebaseapp.com",
+    databaseURL: "https://my-project-1574068747683.firebaseio.com",
+    projectId: "my-project-1574068747683",
+    storageBucket: "my-project-1574068747683.appspot.com",
+    messagingSenderId: "259666193220",
+    appId: "1:259666193220:web:688dadb342a6191533c511",
+    measurementId: "G-FYXE7NEFRR"
+}
